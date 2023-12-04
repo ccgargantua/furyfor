1629a134380f9d8b1ae7a0da136662ed3e57f25d
ccgargantua <lovelygargantua@gmail.com>
2023-12-04 11:29:36 -0500

    windows porting note

--- include/compiler.h
+++ include/compiler.h
@@ -12,7 +12,12 @@
     #define ff_DEPRECATED(msg) __attribute__((deprecated(msg)))
     #define ff_UNIMPLEMENTED_TYPE(t) typedef struct {} t __attribute__((deprecated("Unimplemented type")))
 
-
+// Porting to windows is going to be an absolute pain (to the surprise of nobody).
+// I am going to try my best to be conscious of this as I write, but at the moment
+// I do not have the time or environment to test this for windows. That means
+// I will be relying on my limited knowledge of potential portability issues that
+// could come up when working on windows compatability at a later date. In short,
+// this will be a giant TODO for the time being.
 #elif defined(_MSC_VER)
 
     #include <windows.h>
